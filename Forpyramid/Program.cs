f787a8078a110ac957feb6e260ad02d6c41021dc
opilane <opilane@HP-CZC0359NNZ>
2024-01-08 14:11:31 +0200

    Add project files.

---
+++ Forpyramid/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Forpyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.WriteLine("loo püramiid tärnidest");
+            Console.Write("------------------------");
+            Console.Write("\n\n");
+            Console.WriteLine("sisesta püramiidi suurus");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2*i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            for (i = n - 1; i >= 1; i--)
+            { 
+             for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+             for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
